15f9560f8d1da6efc576907743033a87db0bee4f
Juan Daniel Martinez <juanda.martinezn@gmail.com>
2026-01-30 20:59:09 -0600

    feat: Add customized city and date to lanyard

--- components/lanyard-with-controls.tsx
+++ components/lanyard-with-controls.tsx
@@ -184,6 +184,8 @@ export default function LanyardWithControls({
         userName={inputValue}
         variant={cardVariant}
         onTextureReady={handleTextureReady}
+        city='guadalajara'
+        date='05.02.2026'
       />
       <Lanyard
         key={textureKey}
